02940315998dddcff4280130b409be309b05b342
Flacko <lesliejoe2k2@gmail.com>
2024-06-06 23:36:39 +0000

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import logo from '../images/logo.svg'
+
+
 function App() {
 
   return (
     <>
-     <div>New React lets geauxx</div>
+    <div>
+      <img src={logo} alt="logo" />
+      <h1>We are launching <span>soon!</span></h1>
+      <p>Subscribe and get notified</p>
+      <div className="submit">
+        <input type="text"
+        placeholder='your email addressn'
+         />
+         <button className='btn'>Notify Me</button>
+      </div>
+    </div>
     </>
   )
 }
